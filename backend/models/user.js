7cd355ca448ce2b432746c491bb4acf180605688
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-04-12 18:03:42 -0700

    adding the login and signup logic

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Must use a valid email address'],
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
